d1d8bf9e9fbf2f15b4d4bca9bba34a04a730d45f
Gustgrayera <poneygamer6@gmail.com>
2023-12-28 10:53:47 -0300

    configurando admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Todos, Post, Comment
+
+admin.site.register(User)
+admin.site.register(Todos)
+admin.site.register(Post)
+admin.site.register(Comment)
