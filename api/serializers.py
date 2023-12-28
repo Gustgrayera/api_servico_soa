fa551ed93ac572aa37c01c61322d271b7ac5a3ab
Gustgrayera <poneygamer6@gmail.com>
2023-12-28 10:40:16 -0300

    modificações, rotas e serializers

---
+++ api/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import User, Todos, Post, Comment
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class TodosSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todos
+        fields = '__all__'
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
--- api/views.py
+++ api/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, Todos, Post, Comment
+from .serializers import UserSerializer, TodosSerializer, PostSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    Permite a manipulação de dados de usuários
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class TodosViewSet(viewsets.ModelViewSet):
+    """
+    Permite a manipulação de dados Todos
+    """
+    queryset = Todos.objects.all()
+    serializer_class = TodosSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    Permite a manipulação de dados de post
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    Permite a manipulação de dados comment
+    """
+    queryset = Comment.objects.all()
+    serializer_class = PostSerializer
